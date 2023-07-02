34062a26328732739714d7e7011f687982313486
Durga Sankar <durgapsv99@gmail.com>
2023-07-02 08:51:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-
 import openai
 import gradio
 
-openai.api_key = "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"
+openai.api_key = "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" #the api key
 
-messages = [{"role": "system", "content": "Personal Assitant"}]
+messages = [{"role": "system", "content": "Personal Assitant"}]  #type of assistant
 
 def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-3.5-turbo", #openai gpt version used 3.5-turbo
         messages=messages
     )
     assistant_reply = response["choices"][0]["message"]["content"]
-    messages.append({"role": "assistant", "content": assistant_reply})
+    messages.append({"role": "assistant", "content": assistant_reply})  #messages and replies 
     return assistant_reply
 
-flagging_directory = r"c:\Users\durga\Desktop\task\gradio"
+flagging_directory = r"c:\Users\durga\Desktop\task\gradio"  #logs stored in local, when flagged
 
-demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Mr. Monkey the Assistant", flagging_dir=flagging_directory)
+demo = gradio.Interface(fn=CustomChatGPT, inputs="text", outputs="text", title="Mr. Monkey the Assistant", flagging_dir=flagging_directory) #tile input and outputs
 
-demo.launch(share=True)
+demo.launch(share=True)  #making it sharable produces a second URL which is valid for 72 hours
